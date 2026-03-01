d445be2afbf998f88996f0be64747a6924e3180b
Matteo Zanella <matteozanella2@gmail.com>
2026-03-01 17:36:01 +0100

    chore: updated testing suite

--- testing/test_main.hpp
+++ testing/test_main.hpp
@@ -1,15 +1,5 @@
 #pragma once
 
-// Include this header in exactly ONE .cpp file (your test runner entry point).
-// It defines main() and hands control to the test registry.
-//
-// Example:
-//   // runner.cpp
-//   #include "testing/test_main.hpp"
-//   #include "interval_test.cpp"
-//   #include "timer_test.cpp"
-//   // ... other test files
-
-#include "test_framework.hpp"
+#include "test_framework.hxx"
 
 auto main() -> int { return ::testing::test_registry::instance().run_all(); }
